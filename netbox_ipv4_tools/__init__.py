b80cb11292d9063d1c70c3e9fef68bec97419416
administrator <mlongo4290@gmail.com>
2023-02-01 16:32:49 +0100

    Include package content. Fix collectstatic issue

--- netbox_ipv4_tools/__init__.py
+++ netbox_ipv4_tools/__init__.py
@@ -4,7 +4,7 @@ class NetBoxIPV4ToolsConfig(PluginConfig):
     name = 'netbox_ipv4_tools'
     verbose_name = ' NetBox IPv4 Tools'
     description = 'Work with ipv4 in NetBox'
-    version = '0.0.2'
+    version = '0.0.3'
     base_url = 'ipv4-tools'
 
 config = NetBoxIPV4ToolsConfig

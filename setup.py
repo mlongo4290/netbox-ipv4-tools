9e78bf2e9418ce75d1b1c885a049644261a31c20
Michael <mlongo4290@gmail.com>
2023-02-01 14:34:19 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,29 @@
+from pathlib import Path
+
 from setuptools import find_packages, setup
 
+readme = Path(__file__).parent / "README.md"
+long_description = readme.read_text()
+
 setup(
-    name='netbox-ipv4-tools',
-    version='0.1',
-    description='Work with ipv4 in NetBox',
+    name="netbox-ipv4-tools",
+    version="0.0.1",
+    description="Work with ipv4 in NetBox",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/mlongo4290/netbox-ipv4-tools",
+    author="Michael Longo",
+    license="Apache 2.0",
     install_requires=[],
     packages=find_packages(),
     include_package_data=True,
-    zip_safe=False,
+    keywords=["netbox-plugin"],
+    classifiers=[
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3 :: Only",
+    ],
+    project_urls={
+        "Source": "https://github.com/mlongo4290/netbox-ipv4-tools",
+    },
 )
